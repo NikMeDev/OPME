a547b0c30689db5903533889a47cfbc7fa985075
NikMeDev <ybrbnf2679@mail.ru>
2019-04-27 11:12:27 +0300

    v0.2

---
+++ main.cpp
@@ -0,0 +1,140 @@
+#include <iostream>
+#include <fstream>
+
+using namespace::std;
+
+int main(int argc,char* argv[]) {
+    if(argc == 1){
+        cout << "Open Password Manager with Encryption v0.2" << endl;
+        cout << "Autor: NikMe" << endl;
+        cout << "Usage:" << endl;
+        cout << "opme add <name of service> - add password" << endl;
+        cout << "opme generate <name of service> - generate new password and add it" << endl;
+        cout << "opme get <name of service> - get password" << endl;
+        cout << "opme rename <name of service> <new name of service> - rename service name" << endl;
+        cout << "opme remove <name of service> - delete password" << endl;
+        return 0;
+    }
+    if(string(argv[1]) == "add") {
+        if(argv[2]) {
+            char password[100];
+            int keyNumber;
+            cout << "Enter keynum:";
+            cin >> keyNumber;
+            cout << "Enter your password for " << argv[2] << ":";
+            cin >> password;
+            for(int i = 0; (i < 100 && password[i] != '\0'); i++) {
+                password[i] = password[i] + keyNumber;
+            }
+            fstream file;
+            file.open(argv[2],fstream::out);
+            if(file.is_open()) {
+                file << password;
+                file.close();
+            } else {
+                cout << "Can't create file " << argv[2] << endl;
+            }
+        } else {
+            cout << "Not enough arguments!" << endl;
+            cout << "Usage:" << endl;
+            cout << "opme add <name of service> - add password" << endl;
+        }
+        return 0;
+    }
+    if(string(argv[1]) == "get") {
+        if(argv[2]) {
+            fstream file;
+            file.open(argv[2]);
+            if(file.is_open()) {
+                char password[100];
+                int keyNumber;
+                cout << "Enter your keynum:";
+                cin >> keyNumber;
+                file >> password;
+                for(int i = 0; (i < 100 && password[i] != '\0'); i++) {
+                    password[i] = password[i] - keyNumber;
+                }
+                cout << "Password for " << argv[2] << ":" << password << endl;
+                file.close();
+            } else {
+                cout << "Can't open file " << argv[2] << endl;
+            }
+        } else {
+            cout << "Not enough arguments!" << endl;
+            cout << "Usage:" << endl;
+            cout << "opme get <name of service> - get password" << endl;
+        }
+        return 0;
+    }
+    if(string(argv[1]) == "remove") {
+        if(argv[2]) {
+            remove(argv[2]);
+            cout << "Password for " << argv[2] << " deleted!" << endl;
+        } else {
+            cout << "Not enough arguments!" << endl;
+            cout << "Usage:" << endl;
+            cout << "opme remove <name of service> - delete password" << endl;
+        }
+        return 0;
+    }
+    if(string(argv[1]) == "rename") {
+        if(argv[2] && argv[3]) {
+            rename(argv[2],argv[3]);
+            cout << "Rename " << argv[2] << " to " << argv[3] << " successful!" << endl;
+        } else {
+            cout << "Not enough arguments!" << endl;
+            cout << "Usage:" << endl;
+            cout << "opme rename <name of service> <new name of service> - rename service name" << endl;
+        }
+        return 0;
+    }
+    if(string(argv[1]) == "generate") {
+        if(argv[2]) {
+            srand(time(NULL));
+        	char * pass = new char[16 + 1];
+        	for (int i = 0; i < 16; ++ i) {
+		        switch(rand() % 3){
+		            case 0:
+			            pass[i] = rand() % 10 + '0';
+			            break;
+		            case 1:
+			            pass[i] = rand() % 26 + 'A';
+			            break;
+		            case 2:
+			            pass[i] = rand() % 26 + 'a';
+	        	}   
+	        }
+	        pass[16] = 0;
+            int keyNumber;
+            cout << "Enter keynum:";
+            cin >> keyNumber;
+            for(int i = 0; (i < 100 && pass[i] != '\0'); i++) {
+                pass[i] = pass[i] + keyNumber;
+            }
+            fstream file;
+            file.open(argv[2],fstream::out);
+            if(file.is_open()) {
+                file << pass;
+                file.close();
+            } else {
+                cout << "Can't create file " << argv[2] << endl;
+            }
+        } else {
+            cout << "Not enough arguments!" << endl;
+            cout << "Usage:" << endl;
+            cout << "opme generate <name of service> - add password" << endl;
+        }
+        return 0;
+    }
+    if(argv[1]){
+        cout << "Command " << argv[1] << " is not found.\nUsage:" << endl;
+        cout << "opme add <name of service> - add password" << endl;
+        cout << "opme get <name of service> - get password" << endl;
+        cout << "opme generate <name of service> - add password" << endl;
+        cout << "opme rename <name of service> <new name of service> - rename service name" << endl;
+        cout << "opme remove <name of service> - delete password" << endl;
+        return 0;
+    }
+    cout << "Unhandlead error" << endl;
+    return 0;
+};
